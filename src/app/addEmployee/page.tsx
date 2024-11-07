57c4cfe5a92e33eb7a83950256c9a732b93a980f
sunil-samra <sunil.samra-CIC@ibm.com>
2024-11-07 14:55:33 +0000

    added add employee page which syncs to REST api and populates database

--- src/app/addEmployee/page.tsx
+++ src/app/addEmployee/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import React, {useEffect} from "react";
-
+import React, {useEffect, useState} from "react";
 
 export default function AddEmployee() {
+    const [status, setStatus] = useState(null);
     function getDate() {
       return (
         useEffect(() => {
@@ -12,13 +12,49 @@ export default function AddEmployee() {
         })
       );
     }
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+
+
+      try 
+      {
+        console.log(event.target);
+        const response = await fetch('/api/employee/new', {
+          method: 'POST',
+          body: formData,
+        })
+
+        if (response.ok)
+        {
+          event.target.reset();
+          setStatus('Form submitted successfully!');
+          event.target.reset();
+        }
+        else 
+        {
+          event.target.reset();
+          setStatus('Error submitting form!');
+          event.target.reset();
+        }
+      }
+      catch (error)
+      {
+        event.target.reset();
+        setStatus('Error submitting form!');
+        event.target.reset();
+      }
+      
+    }
+    
     return (
     <div>
       <main>
         <div>
           <h1>Add Employee</h1>
           {getDate()}
-          <form>
+          <form onSubmit={onSubmit} id="formElem">
             <label>First name</label>
             <input type="text" id="firstName" name="firstName" required /><br />
             <label>Middle name</label>

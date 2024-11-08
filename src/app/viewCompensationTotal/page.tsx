dfb5ec4f986ca63e45cf221d55980d8077cb9db0
sunil-samra <sunil.samra-CIC@ibm.com>
2024-11-08 16:37:16 +0000

    completed the view compensation link to backend

--- src/app/viewCompensationTotal/page.tsx
+++ src/app/viewCompensationTotal/page.tsx
@@ -1,23 +1,94 @@
 'use client'
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
+import { formatDateMonthToFullDate } from "../../../lib/functions";
 
 
 export default function viewCompensationTotal() {
     const router = useRouter();
+    const [data, setData] = useState("");
+
+    async function submitForm(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        // submit form without clearing fields
+        var startDate = document.forms["viewCompensationTotal"]["startDate"].value;
+        var endDate = document.forms["viewCompensationTotal"]["endDate"].value;
+        // Needs to be NOT hardcoded
+        var employeeId = '1';
+        
+        startDate = formatDateMonthToFullDate(startDate);
+        endDate = formatDateMonthToFullDate(endDate);
+
+        var apiCall = `http://localhost:3000/api/compensations/search?`;
+        console.log("HOW DOES THIS WORK");
+        let firstParam = true; 
+        if (startDate && !(startDate.length === 0))
+        {
+            firstParam = false;
+            apiCall += `startDate=${startDate}`;
+        }
+        if (endDate && firstParam == false && !(endDate.length === 0))
+        {
+            apiCall += `&endDate=${endDate}`;
+        }
+        else if (endDate && firstParam == true && !(endDate.length === 0))
+        {
+            apiCall += `endDate=${endDate}`;
+        }
+        if (employeeId && firstParam == false && !(employeeId.length === 0))
+        {
+            apiCall += `&employeeId=${employeeId}`;
+        }
+        else if (employeeId && firstParam == true && !(employeeId.length === 0))
+        {
+            apiCall += `employeeId=${employeeId}`;
+        }
+        
+        try 
+        {
+            const response = await fetch(apiCall, 
+                {
+                    method: 'GET'
+                }
+            );
+            const data = await response.json();
+            console.log("IS THIS DATA",data);
+
+            if (response.ok && JSON.stringify(data) != '[]')
+            {
+                console.log("did we get here");
+                setData(JSON.stringify(data));
+            }
+            else if (JSON.stringify(data) == '[]')
+            {
+                setData('0 results found');
+            }
+            else
+            {
+                setData('Error completing search!');
+            }
+
+        }
+        catch (error)
+        {
+            setData('Error completing search!');
+        }
+        //clearForm();
+    }
+
     return (
         <div>
             <main>
                 <div>
                     <h1>View Compensation</h1>
-                    <form id="view compensation total">
+                    <form id="viewCompensationTotal" onSubmit={submitForm}>
                         <label>Start Date YYYY-MM</label>
                         <input type="month" id="startDate" name="startDate" pattern="[0-9]{4}-[0-9]{2}" required/><br />
                         <label>End Date YYYY-MM</label>
                         <input type="month" id="endDate" name="endDate" pattern="[0-9]{4}-[0-9]{2}" required/><br />
                         <input type="submit" value="View Total Compensation" />
                     </form>
-                    <p id="result"></p>
+                    <p id="result">{ data }</p>
                     <button onClick={() => router.push('/')}>Home</button>
                 </div>
             </main>
